3ce8f2340d1db4393e1bf5c972d363cb338d66ce
Shourya Shukla <atulshuklaa605@gmail.com>
2026-08-26 15:07:04 +0530

    Time: 3 ms (26.70%) | Memory: 9.1 MB (35.26%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        vector<int> a; 
+        string b;
+        int mini = INT_MAX; 
+        
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '1') a.push_back(i);
+        }  
+        
+        if(a.size() < k) return ""; 
+        
+        k--;
+        for(int i = 0 ; i < a.size() - k; i++){
+           int len;
+            len = a[i+k] - a[i] + 1;
+            string temp = s.substr(a[i] , len);
+            if(b == "" || len<mini){
+             mini = len;
+             b = temp;
+            }
+            else if(len == mini){
+               if(b>temp){
+                b = temp;
+               }
+            }
+          
+            
+        }
+        return b;
+    }
+};
