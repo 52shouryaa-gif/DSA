ec79e1b5cc6b3107cc8b3fd66a9cbf13bc8d9a52
Shourya Shukla <atulshuklaa605@gmail.com>
2026-09-16 10:44:17 +0530

    Time: 38 ms (14.80%) | Memory: 18 MB (5.02%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int sum(string s , int k){
-        int l = 0; 
-        int r = 0 ; 
+    int sum(string s, int k) {
+        int l = 0;
+        int r = 0;
         int n = s.size();
         int maxlen = 0;
 
-        unordered_map<char , int> mpp;
-        while(r<n){
-        mpp[s[r]]++;
-        while(mpp.size()>k){
-            mpp[s[l]]--;
-            if(mpp[s[l]] == 0) mpp.erase(s[l]);
-            l++;
-        }
-        maxlen += r-l+1;
-        r++;
+        unordered_map<char, int> mpp;
+        while (r < n) {
+            mpp[s[r]]++;
+            while (mpp.size() > k) {
+                mpp[s[l]]--;
+                if (mpp[s[l]] == 0){
+                    mpp.erase(s[l]);
+                }
+                l++;
+            }
+            maxlen += r - l + 1;
+            r++;
         }
         return maxlen;
     }
     int numberOfSubstrings(string s) {
-        int a = sum( s , 3);
-        int b = sum( s , 2);
-        return a-b;
+        int a = sum(s, 3);
+        int b = sum(s, 2);
+        return a - b;
     }
 };
