89d08fac24a70f15851b8124117eb553e87f2249
Shourya Shukla <atulshuklaa605@gmail.com>
2026-09-18 14:38:31 +0530

    Time: 4 ms (38.02%) | Memory: 71.3 MB (39.93%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,23 +3,25 @@ public:
     int candy(vector<int>& ratings) {
 
         vector<int> arr(ratings.size() , 0);
-        vector<int> rrr(ratings.size() , 0);
+     
         arr[0] = 1;
-        rrr[ratings.size()-1] = 1;
+       
         int n = ratings.size();
         int cnt = 0;
         for(int i = 1 ; i < n; i++){
            if(ratings[i]>ratings[i-1]) arr[i] = arr[i-1] + 1;
            else arr[i] = 1;
         }
+        int cur = 1;
+         cnt += max(arr[ratings.size()-1] , 1);
         for(int i = n-2 ; i>=0; i--){
-           if(ratings[i]>ratings[i+1]) rrr[i] = rrr[i+1] + 1;
-           else rrr[i] = 1;
+           if(ratings[i]>ratings[i+1]){
+            cur++;
+           }
+           else cur = 1;
+           cnt+= max(cur , arr[i]);
         }
-        for(int i = 0 ; i < n; i++){
-            int t = max(arr[i] , rrr[i]);
-            cnt += t;
-        }
+      
         return cnt;
     }
 };
