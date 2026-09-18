688430090050e8ce62ae9bc496ebdda6c231cb02
Shourya Shukla <atulshuklaa605@gmail.com>
2026-09-18 14:39:14 +0530

    Time: 5 ms (26.64%) | Memory: 71.4 MB (18.13%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
 
-        vector<int> arr(ratings.size() , 0);
-     
+        vector<int> arr(ratings.size(), 0);
+
         arr[0] = 1;
-       
+
         int n = ratings.size();
-        int cnt = 0;
-        for(int i = 1 ; i < n; i++){
-           if(ratings[i]>ratings[i-1]) arr[i] = arr[i-1] + 1;
-           else arr[i] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1])
+                arr[i] = arr[i - 1] + 1;
+            else
+                arr[i] = 1;
         }
         int cur = 1;
-         cnt += max(arr[ratings.size()-1] , 1);
-        for(int i = n-2 ; i>=0; i--){
-           if(ratings[i]>ratings[i+1]){
-            cur++;
-           }
-           else cur = 1;
-           cnt+= max(cur , arr[i]);
+      int  cnt = max(arr[ratings.size() - 1], 1);
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                cur++;
+            } else
+                cur = 1;
+            cnt += max(cur, arr[i]);
         }
-      
+
         return cnt;
     }
 };
