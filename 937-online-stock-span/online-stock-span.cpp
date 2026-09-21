7b36e1d0f40e1ece1b4b8218828edcd017973f95
Shourya Shukla <atulshuklaa605@gmail.com>
2026-09-21 21:01:20 +0530

    Time: 36 ms (43.25%) | Memory: 97 MB (40.37%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,22 +1,18 @@
 class StockSpanner {
 public:
-stack<pair <int , int>> st;
+stack<pair<int , int>> st;
     StockSpanner() {
         
     }
     
     int next(int price) {
-        
-        int cnt = 1;
-       
-        while(!st.empty() && price>= st.top().first ){
-           
-
-           cnt+= st.top().second;
-             st.pop();
-        }
-         st.push({price , cnt});
-        return cnt;
+       int cnt = 1;
+       while(!st.empty() && st.top().first <= price){
+        cnt += st.top().second;
+        st.pop();
+       } 
+       st.push({price , cnt});
+       return cnt;
     }
 };
 
