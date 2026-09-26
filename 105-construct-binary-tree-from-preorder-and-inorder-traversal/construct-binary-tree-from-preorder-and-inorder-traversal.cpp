8c749167b8ee0ff18b25a5b479a291b000c69711
Shourya Shukla <atulshuklaa605@gmail.com>
2026-09-26 13:06:55 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (15.99%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,32 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int , int>mpp;
+        map<int, int> mpp;
         int i = 0;
-        for(auto it : inorder){
+        for (auto it : inorder) {
             mpp[it] = i;
             i++;
         }
-        TreeNode* root = buildTree(preorder , 0 , preorder.size() - 1 , inorder , 0 , inorder.size() - 1 , mpp);
+        TreeNode* root = buildTree(preorder, 0, preorder.size() - 1, inorder, 0,
+                                   inorder.size() - 1, mpp);
         return root;
     }
-     TreeNode* buildTree(vector<int>& preorder,int prestart , int preend , vector<int>& inorder ,
-     int instart , int inend , map<int , int>& mpp){
-        if(prestart>preend || instart>inend) return NULL;
+    TreeNode* buildTree(vector<int>& preorder, int prestart, int preend,
+                        vector<int>& inorder, int instart, int inend,
+                        map<int, int>& mpp) {
+        if (prestart > preend || instart > inend)
+            return NULL;
         TreeNode* root = new TreeNode(preorder[prestart]);
-        int inroot = mpp[root -> val];
-        int numsleft = inroot - instart ;
-        root -> left =  buildTree( preorder,prestart + 1  ,prestart + numsleft,  inorder ,
-     instart , instart + numsleft - 1 ,mpp);
-      root -> right =  buildTree( preorder,prestart+ numsleft + 1  ,preend,  inorder ,
-     instart + numsleft + 1 , inend ,mpp);
-     return root;
-     }
-     
+        int inroot = mpp[root->val];
+        int numsleft = inroot - instart;
+        root->left = buildTree(preorder, prestart + 1, prestart + numsleft,
+                               inorder, instart, instart + numsleft - 1, mpp);
+        root->right = buildTree(preorder, prestart + numsleft + 1, preend,
+                                inorder, instart + numsleft + 1, inend, mpp);
+        return root;
+    }
 };
